700f0326b790fce9dda43c90fdb69208fbc63124
Husen <saidinahusen10.hs@gmail.com>
2023-04-13 20:43:08 +0700

    create add form course page

---
+++ src/pagess/AddCourse/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Button, ButtonGroup, Form } from 'react-bootstrap'
+import { StyledContainer, StyledTitle } from './style'
+import FormInput from '../../components/FormInput'
+import useAddCourseState from './useAddCouseState'
+
+const FORM_LIST = [
+    { id: "title", label: "TItle", type: "text", placeholder: "Enter course title" },
+    { id: "description", label: "Description", type: "textarea", placeholder: "Enter course description" },
+    { id: "typeId", label: "Type Id", type: "text", placeholder: "Enter course type id" },
+    { id: "level", label: "Level", type: "text", placeholder: "Enter course level" },
+    { id: "duration", label: "Duration", type: "text", placeholder: "Enter course duration" }
+]
+
+const AddCourse = () => {
+    const { getter, setter } = useAddCourseState()
+
+  return (
+    <StyledContainer>
+        <StyledTitle>Add Course</StyledTitle>
+        <Form>
+            { FORM_LIST.map(item => (
+                <FormInput 
+                    label={item.label}
+                    type={item.type}
+                    value={getter[item.id]}
+                    onChange={setter[item.id]}
+                    placeholder={item[item.placeholder]}
+                />
+            )) }
+            <ButtonGroup>
+                <Button variant='success'>
+                    Submit
+                </Button>
+                <Button variant='secondary'>
+                    Cancel
+                </Button>
+            </ButtonGroup>
+        </Form>
+    </StyledContainer>
+  )
+}
+
+export default AddCourse
