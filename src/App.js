0fb8ad042706cf92d1a73317bc362209dac470b2
Husen <saidinahusen10.hs@gmail.com>
2023-04-14 15:03:31 +0700

    belajar performance render di react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// input address
+const ChildA = ({setAddress, address}) => {
+    console.log('child a')
+
+    return (
+        <>
+            <p>child a component</p>
+            <form>
+                <input placeholder={"address"} onChange={(e) => setAddress(e.target.value)} value={address} />
+            </form>
+        </>
+    )
+}
+
+// input name
+const ChildB = ({setName, name}) => {
+    console.log('child b')
+    return (
+        <>
+            <p>child b component</p>
+            <form>
+                <input placeholder={"name"} onChange={(e) => setName(e.target.value)} value={name} />
+            </form>
+        </>
+    )
+}
+
+// print
+const ChildC = React.memo((props) => {
+    console.log('child c')
+    return (
+        <>
+            <p>child c component</p>
+            <p>name : {props.name}</p>
+        </>
+    )
+})
+
+// const ChildD = () => {
+
+// }
+
+const ChildE = React.memo(({address}) => {
+    console.log('child e')
+    return (
+        <p>address: {address}</p>
+    )
+})
+
+const Parent = () => {
+    console.log("render parent component")
+    const [name, setName] = React.useState("")
+    const [address, setAddress] = React.useState("")
+    return (
+        <>
+            <ChildA address={address} setAddress={setAddress} />
+            <ChildB name={name} setName={setName} />
+            <ChildC name={name} />
+            <ChildE address={address} />
+        </>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Parent />
+    </>
   );
 }
 
