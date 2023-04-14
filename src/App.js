6c17a8b77f6baf95f89a2b436d163478bcd86dd5
Husen <saidinahusen10.hs@gmail.com>
2023-04-14 22:38:49 +0700

    belajar use state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const ChildA = () => {
+  const [name, setName] = React.useState("");
+  console.log("render child A component");
+
+  return (
+    <>
+      <p>childA</p>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <ChildC name={name}/>
+    </>
+  );
+};
+
+const ChildB = () => {
+  const [age, setAge] = React.useState(0);
+  console.log("render child B");
+
+  return (
+    <>
+      <p>childB</p>
+      <input
+        type="number"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <ChildD age={age} />
+    </>
+  );
+};
+
+const ChildC = (props) => {
+    console.log("render child C")
+    return (
+        <>
+            <p>name : {props.name}</p>
+        </>
+    )
+}
+
+const ChildD = (props) => {
+    console.log("render child D")
+    return (
+        <>
+            <p>age : {props.age}</p>
+        </>
+    )
+}
+
+const Parent = () => {
+  console.log("render parent component");
+  return (
+    <>
+      <h1>Parent component</h1>
+      <ChildA />
+      <ChildB />
+    </>
+  );
+};
+
 
 function App() {
+  console.log("render app component");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Parent />
+    </>
   );
 }
 
